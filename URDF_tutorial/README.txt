f15f747059bcabbf7ac5ba4f133716b4759e0ec4
son6966 <son6966rz@gmail.com>
2024-11-19 15:38:49 +0700

    add urdf

--- URDF_tutorial/README.txt
+++ URDF_tutorial/README.txt
@@ -5,3 +5,6 @@ base on: ROS 2 Humble Hawksbill
 Tutorial to create a package
 
 sudo apt install ros-humble-urdf-tutorial
+
+run:
+ros2 launch urdf_tutorial display.launch.py model:=urdf/07-physics.urdf
---
+++ URDF_tutorial/build/urdf_tutorial/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/son6966/Desktop/AML/Ros2_tutorial/URDF_tutorial/src/urdf
+# Build directory: /home/son6966/Desktop/AML/Ros2_tutorial/URDF_tutorial/build/urdf_tutorial
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
